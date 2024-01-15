9a606a00b49dadbfe2e2b86cd2fb173017e4a399
dimpleyadav <dimpleyadav32@gmail.com>
2024-01-16 00:35:02 +0530

    first commit

---
+++ src/pages/User.js
@@ -0,0 +1,23 @@
+
+import "../styles/User";
+
+function User() {
+  return (
+    <>
+       <div className="User">
+    <h6>TOPIC</h6>
+    <h1>Cats</h1>
+
+    <div className="upload" >
+        <img src="images/uploadicon.jpg" alt="upload" />
+    </div>
+    <h2>Meme tagline</h2>
+
+    <button>Upload</button>
+    </div>
+    
+  </>
+  );
+}
+
+export default User;
